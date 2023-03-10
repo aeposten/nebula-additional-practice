cca619ee10856a318d640abdac2224d93cb0cdb9
Amy <86963506+aeposten@users.noreply.github.com>
2023-03-10 15:10:50 -0800

    Practiced writing tests

--- amy-test-practice/learn-vitest/sum.js
+++ amy-test-practice/learn-vitest/sum.js
@@ -1,9 +1,6 @@
-function sum (numArr) {
-   const sumOfArray = numArr.reduce((a, b) => a + b)
-   return sumOfArray;
+function sum(numArr=[]) {
+  const sumOfArray = numArr.reduce((a, b) => a + b, 0);
+  return sumOfArray;
 }
 
-const numbers = [2, 4, 6, 8]
-
-console.log(sum(numbers))
 export default sum;
