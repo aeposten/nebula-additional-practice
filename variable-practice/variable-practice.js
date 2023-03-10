4a41720614916c7b3cb88c05544f5972d5e9814c
Amy <86963506+aeposten@users.noreply.github.com>
2023-03-10 15:27:15 -0800

    Added test files to variable practice

---
+++ variable-practice/variable-practice.js
@@ -0,0 +1,17 @@
+// Declare a variable, favoriteAnimal, and use it to store the name of your favorite animal as a string.
+const favoriteAnimal = "giraffe";
+
+// Declare a variable favoriteCandy, and use it to store the name of your favorite candy as a string. Next declare a variable, candyAmount use it to store the number of pieces of candy (any number you want) as a number. Then log these to the console.
+const favoriteCandy = "Jolly Ranchers";
+let candyAmount = 7;
+
+// Now, pretend you ate some of the candy, and reassign the candyAmount variable to a new number and log that to the console.
+candyAmount = 4;
+
+// You just got some more candy! declare a variable moreCandy and use it to store a new number of pieces of candy (any number you want) as a number.
+const moreCandy = 27;
+
+// Finally, declare a variable, totalCandy, and use it to store the value of candyAmount added to moreCandy. Log the final value to the console.
+const totalCandy = candyAmount + moreCandy;
+
+export { favoriteAnimal, favoriteCandy, candyAmount, moreCandy, totalCandy };
---
+++ variable-practice/variable-practice.test.js
@@ -0,0 +1,10 @@
+import { describe, expect, it } from "vitest"
+
+import {
+  favoriteAnimal,
+  favoriteCandy,
+  candyAmount,
+  moreCandy,
+  totalCandy,
+} from "./variable-practice";
+
