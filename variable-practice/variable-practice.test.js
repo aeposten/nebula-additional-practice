f24bc12c9c7bb597edec56b5d77baf9e81e98695
Amy <86963506+aeposten@users.noreply.github.com>
2023-03-10 15:55:08 -0800

    completed tests for variable-practice

--- variable-practice/variable-practice.test.js
+++ variable-practice/variable-practice.test.js
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "vitest"
+import { describe, expect, it } from "vitest";
 
 import {
   favoriteAnimal,
@@ -8,3 +8,32 @@ import {
   totalCandy,
 } from "./variable-practice";
 
+describe("strings", () => {
+  it("should be a string data type", () => {
+    expect(typeof favoriteAnimal).toBe("string");
+  });
+
+  it("should be a string data type", () => {
+    expect(typeof favoriteCandy).toBe("string");
+  });
+});
+
+describe("numbers", () => {
+  it("should be a number data type", () => {
+    expect(typeof candyAmount).toBe("number");
+  });
+
+  it("should be a number data type", () => {
+    expect(typeof moreCandy).toBe("number");
+  });
+
+  it("should be a number data type", () => {
+    expect(typeof totalCandy).toBe("number");
+  });
+
+  describe("operations", () => {
+    it("totalCandy should be the sum of candyAmount and moreCandy", () => {
+      expect(totalCandy).toBe(candyAmount + moreCandy)
+    });
+  });
+});
