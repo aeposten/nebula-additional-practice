e93e3c2158c8c661235c458bf49f4cace262a832
Amy <86963506+aeposten@users.noreply.github.com>
2023-03-12 13:31:10 -0700

    FInised tests for numbers practice 1

---
+++ from-nebula-content/numbers-practice-1/numbers-practice-1.js
@@ -0,0 +1,58 @@
+// a variable num1, and assign it to a number. Then reassign it to the previous value multiplied by 2.
+// Double that value and re-assign it to the previous variable
+let num1 = 4;
+num1 *= 2;
+
+// Two variables a, and b and assign them to numbers. Then declare a third variable, sum and assign it to a and b added together.
+const a = 20;
+const b = 40;
+
+const sum = a + b;
+
+// Declard 3 variables myVar1, myVar2, and myVar3, and assign them to numbers. Then declare a third variable, difference and assign it to the 3 numbers subtracted  from eachother.
+
+const myVar1 = 59;
+const myVar2 = 20;
+const myVar3 = 8;
+
+const difference = myVar1 - myVar2 - myVar3;
+
+// Declare two variables, even1 and even2, and assign them to even numbers, then declare a third variable divided, and assign it to even1 divided by even2.
+const even1 = 22;
+const even2 = 44;
+
+const divided = even1 / even2;
+
+// Declare two variables odd1 and odd2, and assign them to odd numbers. Then, declare a third variable multiplied, and assign it to odd1 multiplied by odd2.
+const odd1 = 3;
+const odd2 = 7;
+
+const multiplied = odd1 * odd2;
+
+// Create a divide3, and assign it to a number. Then, declare another variable, remainder, and assign it to the remainder of divide3 divided by 3.
+const divide3 = 26;
+const remainder = 26 % 3;
+
+// Answer in plain text, what is modulo especially useful for?
+
+
+
+// Do not edit below this line
+export {
+  num1,
+  a,
+  b,
+  sum,
+  myVar1,
+  myVar2,
+  myVar3,
+  difference,
+  even1,
+  even2,
+  divided,
+  odd1,
+  odd2,
+  multiplied,
+  divide3,
+  remainder,
+};
---
+++ from-nebula-content/numbers-practice-1/numbers-practice-1.test.js
@@ -0,0 +1,152 @@
+import { describe, expect, it } from "vitest";
+
+import {
+  num1,
+  a,
+  b,
+  sum,
+  myVar1,
+  myVar2,
+  myVar3,
+  difference,
+  even1,
+  even2,
+  divided,
+  odd1,
+  odd2,
+  multiplied,
+  divide3,
+  remainder,
+} from "./numbers-practice-1";
+
+describe("num1", () => {
+  it("should be data type number", () => {
+    expect(typeof num1).toBe("number");
+  });
+});
+
+describe("a", () => {
+  it("should be data type number", () => {
+    expect(typeof a).toBe("number");
+  });
+});
+
+describe("b", () => {
+  it("should be data type number", () => {
+    expect(typeof a).toBe("number");
+  });
+});
+
+describe("sum", () => {
+  it("should be data type number", () => {
+    expect(typeof sum).toBe("number");
+  });
+
+  it("should be data type number", () => {
+    expect(sum).toEqual(a + b);
+  });
+});
+
+describe("myVar1", () => {
+  it("should be data type number", () => {
+    expect(typeof myVar1).toBe("number");
+  });
+});
+
+describe("myVar2", () => {
+  it("should be data type number", () => {
+    expect(typeof myVar1).toBe("number");
+  });
+});
+
+describe("myVar3", () => {
+  it("should be data type number", () => {
+    expect(typeof myVar1).toBe("number");
+  });
+});
+
+describe("difference", () => {
+  it("should be data type number", () => {
+    expect(typeof difference).toBe("number");
+  });
+
+  it("should be equal to myVar1 - myVar2 - myVar3", () => {
+    expect(difference).toEqual(myVar1 - myVar2 - myVar3);
+  });
+});
+
+describe("even1", () => {
+  it("should be data type number", () => {
+    expect(typeof even1).toBe("number");
+  });
+
+  it("should be an even number", () => {
+    expect(even1 % 2).toEqual(0);
+  });
+});
+
+describe("even2", () => {
+  it("should be data type number", () => {
+    expect(typeof even2).toBe("number");
+  });
+
+  it("should be an even number", () => {
+    expect(even2 % 2).toEqual(0);
+  });
+});
+
+describe("divided", () => {
+  it("should be data type number", () => {
+    expect(typeof divided).toBe("number");
+  });
+
+  it("should be equal to even1 divided by even2", () => {
+    expect(divided).toEqual(even1 / even2);
+  });
+});
+
+describe("odd1", () => {
+  it("should be data type number", () => {
+    expect(typeof odd1).toBe("number");
+  });
+
+  it("should be an odd1 number", () => {
+    expect(odd1 % 2).not.toEqual(0);
+  });
+});
+
+describe("odd2", () => {
+  it("should be data type number", () => {
+    expect(typeof odd2).toBe("number");
+  });
+
+  it("should be an odd number", () => {
+    expect(odd2 % 2).not.toEqual(0);
+  });
+});
+
+describe("multiplied", () => {
+  it("should be data type number", () => {
+    expect(typeof multiplied).toBe("number");
+  });
+
+  it("should be an odd number", () => {
+    expect(multiplied).toEqual(odd1 * odd2);
+  });
+});
+
+describe("divide3", () => {
+  it("should be data type number", () => {
+    expect(typeof divide3).toBe("number");
+  });
+});
+
+describe("remainder", () => {
+  it("should be data type number", () => {
+    expect(typeof remainder).toBe("number");
+  });
+
+  it("should be an odd number", () => {
+    expect(remainder).toEqual(divide3 % 3);
+  });
+});
