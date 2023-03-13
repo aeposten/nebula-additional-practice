6c815a696bd4129f249f1e872b6ac0677b041ea9
Amy <86963506+aeposten@users.noreply.github.com>
2023-03-13 12:57:47 -0700

    Completed tests for numbers-hw

---
+++ from-nebula-content/numbers/numbers-hw/numbers-hw.js
@@ -0,0 +1,80 @@
+// 1. Declare a variable, myVar,  and assign it to a numeric value. Then half that value and re-assign it to the previous variable
+let myVar = 12;
+
+myVar = myVar / 2;
+
+// 2. Create two variables, neg1 and neg2, and assign them to negative numbers negative numbers, then create a third variable, difference, and set it equal to neg1 subtracted from neg2
+const neg1 = -6;
+const neg2 = -12;
+const difference = neg1 - neg2;
+
+// 3. Create three variables, num1, num2, and num3, and assign numbers to them. Then, declare a third variable, multiplied, and set it equal to the previous three variables multiplied together.
+const num1 = 2;
+const num2 = 4;
+const num3 = 6;
+const multiplied = num1 * num2 * num3;
+
+// 4. Declare two variables, even1 and even2, and assign them to even numbers. Then, declare a third variable, divideEven, and set it to even1 divided by even2.
+const even1 = 24;
+const even2 = 12;
+const divideEven = even1 / even2;
+
+// 5. Declare two variables, odd1 and odd2, and assign them to odd numbers. Then, declare a third variable, divideOdd, and set it to odd1 divided by odd2.
+const odd1 = 9;
+const odd2 = 3;
+const divideOdd = odd1 / odd2;
+
+// 6. Declare a variable, minVal and set it equal to the minimum value in the following set of numbers: 10,20,30,40,50
+
+const minVal = Math.min(10, 20, 30, 40, 50);
+
+// 7. Declare a variable, maxVal and set it equal to the maximum value in the following set of numbers:  -1,-2,-3,-4,-5
+const maxVal = Math.max(-1, -2, -3, -4, -5);
+
+// 8 .Create a variable, myNum and set it equal to a number, then declare a second variable, remainder, and set it equal to the remainder of myNum divided by 2.
+const myNum = 79;
+const remainder = myNum % 2;
+
+// 9. Declare a variable squareRoot and set it equal to the square root of 144
+
+const squareRoot = Math.sqrt(144);
+
+// 10. Declare a variable, threeToSeven, and set it equal to the value of 3 to the power of 7
+const threeToSeven = Math.pow(3, 7);
+
+// 11. Declare a variable, rounded, and set it equal to the value of 73.2 rounded
+const rounded = Math.round(73.2);
+
+// 12. Declare a variable, roundedDown, and set it equal to the rounded-down value of 9.9
+const roundedDown = Math.floor(9.9);
+
+// 13. Declare a variable, absVal, and set it equal to the absolute value of -1231
+const absVal = Math.abs(-1231);
+
+
+// DO NOT EDIT BELOW THIS LINE
+export {
+  myVar,
+  neg1,
+  neg2,
+  difference,
+  num1,
+  num2,
+  num3,
+  multiplied,
+  even1,
+  even2,
+  divideEven,
+  odd1,
+  odd2,
+  divideOdd,
+  minVal,
+  maxVal,
+  myNum,
+  remainder,
+  squareRoot,
+  threeToSeven,
+  rounded,
+  roundedDown,
+  absVal,
+};
---
+++ from-nebula-content/numbers/numbers-hw/numbers-hw.test.js
@@ -0,0 +1,167 @@
+import { describe, expect, it } from "vitest";
+
+import {
+  myVar,
+  neg1,
+  neg2,
+  difference,
+  num1,
+  num2,
+  num3,
+  multiplied,
+  even1,
+  even2,
+  divideEven,
+  odd1,
+  odd2,
+  divideOdd,
+  minVal,
+  maxVal,
+  myNum,
+  remainder,
+  squareRoot,
+  threeToSeven,
+  rounded,
+  roundedDown,
+  absVal,
+} from "./numbers-hw";
+
+describe("Question 1", () => {
+  describe("myVar", () => {
+    it("Should have the data type number", () => {
+      expect(typeof myVar).toBe("number");
+    });
+  });
+});
+
+describe("Question 2", () => {
+  describe("neg1", () => {
+    it("should be a negative number", () => {
+      expect(neg1).toBeLessThan(0);
+    });
+  });
+
+  describe("neg2", () => {
+    it("should be a negative number", () => {
+      expect(neg2).toBeLessThan(0);
+    });
+  });
+
+  describe("difference", () => {
+    it("Should equal neg1 minus neg2", () => {
+      expect(difference).toStrictEqual(neg1 - neg2);
+    });
+  });
+});
+
+describe("Question 3", () => {
+  describe("multiplied", () => {
+    it("Should equal num1 multiplied by num2 multiplied by num3", () => {
+      expect(multiplied).toStrictEqual(num1 * num2 * num3);
+    });
+  });
+});
+
+describe("Question 4", () => {
+  describe("even1", () => {
+    it("Should be an even number", () => {
+      expect(even1 % 2).toStrictEqual(0);
+    });
+  });
+
+  describe("even2", () => {
+    it("Should be an even number", () => {
+      expect(even2 % 2).toStrictEqual(0);
+    });
+  });
+
+  describe("divideEven", () => {
+    it("Should equal even1 divided by even2", () => {
+      expect(divideEven).toStrictEqual(even1 / even2);
+    });
+  });
+});
+
+describe("Question 5", () => {
+  describe("odd1", () => {
+    it("Should be an odd number", () => {
+      expect(odd1 % 2).toStrictEqual(1);
+    });
+  });
+
+  describe("odd2", () => {
+    it("Should be an odd number", () => {
+      expect(odd2 % 2).toStrictEqual(1);
+    });
+  });
+
+  describe("divideOdd", () => {
+    it("Should equal even1 divided by even2", () => {
+      expect(divideOdd).toEqual(odd1 / odd2);
+    });
+  });
+});
+
+describe("Question 6", () => {
+  describe("minVal", () => {
+    it("Should return the minimum value in the set of numbers", () => {
+      expect(minVal).toStrictEqual(Math.min(10, 20, 30, 40, 50));
+    });
+  });
+});
+
+describe("Question 7", () => {
+  describe("maxVal", () => {
+    it("Should return the maximum value in the set of numbers", () => {
+      expect(maxVal).toStrictEqual(Math.max(-1, -2, -3, -4, -5));
+    });
+  });
+});
+
+describe("Question 8", () => {
+  describe("remainder", () => {
+    it("Should be equal to the remainder of myNum divided by 2", () => {
+      expect(remainder).toStrictEqual(myNum % 2);
+    });
+  });
+});
+
+describe("Question 9", () => {
+  describe("squareRoot", () => {
+    it("Should be equal to the square root of 144", () => {
+      expect(squareRoot).toStrictEqual(Math.sqrt(144));
+    });
+  });
+});
+
+describe("Question 10", () => {
+  describe("threeToSeven", () => {
+    it("Should equal three to the power of 7", () => {
+      expect(threeToSeven).toStrictEqual(Math.pow(3, 7));
+    });
+  });
+});
+
+describe("Question 11", () => {
+  describe("rounded", () => {
+    it("Should equal 73.2 rounded", () => {
+      expect(rounded).toStrictEqual(Math.round(73.2));
+    });
+  });
+});
+
+describe("Question 12", () => {
+  describe("rounded", () => {
+    it("Should equal 9.9 rounded down", () => {
+      expect(roundedDown).toStrictEqual(Math.floor(9.9));
+    });
+  });
+});
+
+describe("Question 13", () => {
+  describe("absVal", () => {
+    it("Should equal the absolute value of -1231", () => {
+      expect(absVal).toStrictEqual(Math.abs(-1231));
+    });
+  });
+});
